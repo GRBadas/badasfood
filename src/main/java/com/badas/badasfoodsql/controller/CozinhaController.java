009ddff26d4e0ba0aaedf60b29701862f6d685e6
Gabriel Badaró <grbadas@gmail.com>
2023-03-08 10:30:24 -0300

    update

--- src/main/java/com/badas/badasfoodsql/controller/CozinhaController.java
+++ src/main/java/com/badas/badasfoodsql/controller/CozinhaController.java
@@ -2,9 +2,19 @@ package com.badas.badasfoodsql.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.badas.badasfoodsql.domain.model.Cozinha;
@@ -20,6 +30,49 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha= cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha != null) {
+		return ResponseEntity.ok(cozinha);	
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if(cozinhaAtual != null) {
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+		return ResponseEntity.ok(cozinhaAtual);
+	}
+		return ResponseEntity.noContent().build();
+}
+	
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		try {
+			
+			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+			
+			if(cozinha != null ) {
+			cozinhaRepository.remover(cozinha); 
+			return ResponseEntity.noContent().build();
+			}
+			return ResponseEntity.notFound().build();
+		} catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
 	}
 
 }
--- src/main/java/com/badas/badasfoodsql/domain/repository/CozinhaRepository.java
+++ src/main/java/com/badas/badasfoodsql/domain/repository/CozinhaRepository.java
@@ -3,9 +3,11 @@ package com.badas.badasfoodsql.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.badas.badasfoodsql.domain.model.Cozinha;
 
-public interface CozinhaRepository {
+public interface CozinhaRepository  extends JpaRepository<Cozinha, Long>{
 
 	List<Cozinha> listar();
 	Cozinha buscar(Long id);
